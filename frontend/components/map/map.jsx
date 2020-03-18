d6b6b10446bb9ff549756ab338738220daeaa366
KaminKevCrew <durgeisawesome@gmail.com>
2020-03-18 16:51:20 -0700

    Add new map component

--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -8,52 +8,116 @@ class showMap extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       map: "",
     }
-
+    this.mapAddPopup = this.mapAddPopup.bind(this);
+    this.mapAddRoute = this.mapAddRoute.bind(this);
+    this.mapAddStyle = this.mapAddStyle.bind(this);
+    this.mapCreate = this.mapCreate.bind(this);
+    this.mapBuilder = this.mapBuilder.bind(this);
     this.handleClick = this.handleClick.bind(this)
+  }
 
+  mapAddPopup(map, data) { // Adds a popup on click for a point of interest.
+    map.on('load', function () {
+      map.addSource('places', data)
+      map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+          'icon-image': '{icon}-15',
+          'icon-allow-overlap': true
+        }
+      });
+      map.on('click', 'places', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'places', function () {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', 'places', function () {
+        map.getCanvas().style.cursor = '';
+      });
+    });
+  }
+
+  mapAddRoute(map, coordinates) {
+    map.on('load', function () { // Adds a source to get coordinates from
+      map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'LineString',
+            'coordinates': coordinates
+          }
+        }
+      }); // add source
+      map.addLayer({ // Adds coordinates to map with selected styling.
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#888',
+          'line-width': 8
+        }
+      }); // add layer
+    }) // on load
 
   }
 
-  componentDidMount() {
-    let zoom = 9
-    const center = [-122.401199, 37.798845]
-
-    mapboxgl.accessToken = 'pk.eyJ1Ijoia2FtaW5rZXZjcmV3IiwiYSI6ImNrMHNlbXRpdjAxd3ozbXFnNWNidnQ4cHkifQ.tRPQTLC7vIb0D9i5Hph3xA';
-
-    this.map = new mapboxgl.Map({
-      container: "map",
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: center, 
-      zoom: zoom,
-      interactive: true
-    });
-
-    let map = this.map;
-
+  mapAddStyle(map) {
     map.on('load', function () {
-      map.addLayer({
-        id: 'rpd_parks',
-        type: 'fill',
-        source: {
-          type: 'vector',
-          url: 'mapbox://mapbox.3o7ubwm8'
-        },
-        'source-layer': 'RPD_Parks',
-        layout: {
-          visibility: 'visible'
-        },
-        paint: {
-          'fill-color': 'rgba(61,153,80,0.55)'
-        }
-      });
-    })
-  };
+      // adds zoom/rotate map controls
+      map.addControl(new mapboxgl.NavigationControl());
+    }) // on load
+  }
 
+  mapCreate(container = "map", center = [-122, 38], zoom = 9, interactive = true) {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia2FtaW5rZXZjcmV3IiwiYSI6ImNrMGUydnoxbzBkbHgzY3IxOGZmcWN6dHAifQ.K2wzWQ-7KEhyUxXFR48aTA';
 
+    let map = new mapboxgl.Map({
+      container: container,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: center,
+      zoom: zoom,
+      interactive: interactive
+    });
+    return map
+  }
+
+  mapBuilder(map) { // Adds each element to the map
+    this.mapAddStyle(map);
+    this.mapAddRoute(map, this.coordinates);
+    this.mapAddPopup(map, this.data);
+  }
+
+  componentDidMount() {
+    let map = this.mapCreate();
+    this.mapBuilder(map);
+
+  }
 
   handleClick() {
     this.props.history.location.pathname === "/feed" ?
