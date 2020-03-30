6f7856667459a277b0e5114ad4945c3684ac8248
KaminKevCrew <durgeisawesome@gmail.com>
2020-03-30 15:10:04 -0700

    Fix Routes: :

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: {format: :json} do
-    resources :routes
-    resources :likes, only: [:index, :create, :show, :destroy]
-    resources :follows, only: :index
-    resources :comments, only: [:index, :show, :create, :destroy]
-    resources :workouts, only: [:index, :show, :create]
-    resources :users, only: [:create, :show]
-    resource :session, only: [:create, :destroy, :show]
+  namespace :api, defaults: { format: :json } do
+
+    resources :users, only: [:create, :show, :index] do 
+      resources :routes, only: [:create, :destroy, :index, :show, :edit]
+    end
+
+    resource :session, only: [:create, :destroy]
+
+    resources :activities do 
+      resources :comments, only: [:index]
+      resources :likes, only: [:index]
+    end
+
+    resources :likes, only: [:create, :show, :destroy]
+
+    resources :comments, only: [:create, :destroy, :show]
+
+    resources :follows, only: [:index]
   end
 
   root "static_pages#root"
